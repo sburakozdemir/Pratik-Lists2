45cd5a5df338dfc0f4353cbf2c89269840e5df85
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-13 14:42:28 +0300

    Add project files.

---
+++ Pratik-Lists2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Ana program
+Console.WriteLine("Kahve Listesi Oluşturucu");
+Console.WriteLine("-------------------------");
+
+// Kahve isimlerini saklamak için bir liste oluştur
+List<string> kahveler = new List<string>();
+
+// Kullanıcıdan 5 kahve ismi al
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"{i}. kahve adını giriniz: ");
+    string kahveAdi = Console.ReadLine();
+
+    // Boş girişleri engelle
+    while (string.IsNullOrWhiteSpace(kahveAdi))
+    {
+        Console.Write("Lütfen geçerli bir kahve adı giriniz: ");
+        kahveAdi = Console.ReadLine();
+    }
+
+    kahveler.Add(kahveAdi);
+}
+
+// Girilen kahve isimlerini ekrana yazdır
+Console.WriteLine("\nGirilen Kahve İsimleri:");
+Console.WriteLine("-------------------------");
+foreach (string kahve in kahveler)
+{
+    Console.WriteLine($"- {kahve}");
+}
+
+Console.WriteLine("\nKahve listeniz hazır. Afiyet olsun!");
